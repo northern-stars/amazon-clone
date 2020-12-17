49e95e7a9535080e0c178e7e6a98a2e4c8896b1d
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-12-17 11:57:46 +0300

    meterial ui/core installed and created header components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
+import Home from "./components/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
-        HELLO
+      <Router>
+        <Switch>
+          <Route path="/"> 
+            <Home/>
+          </Route>
+        </Switch>
+
+
+      </Router>
+        
     </div>
   );
-}
+};
 
 export default App;
