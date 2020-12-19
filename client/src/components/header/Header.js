9679bb8cce524e13a5f0a4c16b487673d8d13cf1
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-12-19 17:36:44 +0300

    collapsable header bar added

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,96 +1,213 @@
 import React from 'react'
 import { AppBar,
          Toolbar, 
-         makeStyles, 
+         makeStyles,
+         fade, 
          InputBase,
          BottomNavigation, 
-         BottomNavigationAction
+         BottomNavigationAction,
+         IconButton,
+         MenuItem,
+         Menu,
         } from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
 
-const useStyles = makeStyles({
-    appBar: {
-        backgroundColor: '#131921',
-        display: 'flex',
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
     },
-    headerLogo: {
-        width: '100px',
-        objectFit: 'contain',
-        marginTop: '10px',
-        marginRight: '1rem', 
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
     },
-    searchInput: {
-        backgroundColor: 'white',
-        paddingLeft: '0.5rem',
-        border: 'none',
-        borderRadius: '6px 0 0 6px'
-        
+  },
+  headerLogo: {
+    width: '100px',
+    objectFit: 'contain',
+    marginTop: '10px',
+    marginRight: '1rem', 
     },
-    searchIcon: {
-        backgroundColor: '#cd9042',
-        height: '22.1px',
-        padding: '5.4px',
-        borderRadius: '0 6px 6px 0'
-    },
-    headerSearch: {
-        display: 'flex',
-        flex: 1,
-        alignItems: 'center',
-    },
-    headerNavigation: {
-        display: 'flex',
-        justifyContent: 'space-evenly',
-        backgroundColor: '#131921',
-        padding: '0px',  
-    },
-    action: {
-        display: 'flex',
-        flexDirection: 'column',
-        color: 'white',
-        fontWeight: 800,
-        padding: '0px',
-        margin: '0px' 
-    } 
-});
+  searchInput: {
+    backgroundColor: 'white',
+    paddingLeft: '0.5rem',
+    border: 'none',
+    borderRadius: '6px 0 0 6px'
+    
+  },
+  searchIcon: {
+    backgroundColor: '#cd9042',
+    height: '22.1px',
+    padding: '5.4px',
+    borderRadius: '0 6px 6px 0'
+  },
+  headerSearch: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+  },
+  headerNavigation: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#131921',
+    padding: '0px',  
+  },
+  action: {
+    display: 'flex',
+    flexDirection: 'column',
+    color: 'white',
+    fontWeight: 800,
+    padding: '0px',
+    margin: '0px' 
+  }, 
 
-function Header() {
-    const classes = useStyles();
+}));
 
-    return (
-        <div className="header">
-            <AppBar className={classes.appBar} position="static" >
-                <Toolbar>
-                    <img
-                        className={classes.headerLogo}
-                        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                        alt="brand logo"
-                    />
-                    <div className={classes.headerSearch}>
-                        <InputBase 
-                            className={classes.searchInput}
-                            margin="none"
-                            placeholder="Search.."
-                            fullWidth
-                        />
-                        <SearchIcon className={classes.searchIcon}/>
-                    </div>
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
 
-                    <BottomNavigation
-                        showLabels
-                        className={classes.headerNavigation}
-                    >
-                        <BottomNavigationAction className={classes.action} label="Sign In" />
-                        <BottomNavigationAction className={classes.action} label="& Orders" />
-                        <BottomNavigationAction className={classes.action} label="Prime" />
-                        <BottomNavigationAction className={classes.action} icon={<ShoppingBasketIcon/>}/>  
-                    </BottomNavigation>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-};
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-export default Header;
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+    >
+        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileMenuClose}
+    >
+        <MenuItem>
+            <p>Sign In/Up</p>
+        </MenuItem>
+        <MenuItem>
+            <p>Log Out</p>
+        </MenuItem>
+        <MenuItem onClick={handleProfileMenuOpen}>
+            <AccountCircle />
+            <p>Profile</p>
+        </MenuItem>
+        </Menu>
+    );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" style={{backgroundColor: '#131921'}}>
+        <Toolbar>
+            <img
+                className={classes.headerLogo}
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="brand logo"
+            />
+            <div className={classes.headerSearch}>
+                <InputBase 
+                    className={classes.searchInput}
+                    margin="none"
+                    placeholder="Search.."
+                    fullWidth
+                />
+                <SearchIcon className={classes.searchIcon}/>
+            </div>
+          
+            <div className={classes.sectionDesktop}>
+                <BottomNavigation
+                    showLabels
+                    className={classes.headerNavigation}
+                >
+                    <BottomNavigationAction className={classes.action} label="Sign In" />
+                    <BottomNavigationAction className={classes.action} label="& Orders" />
+                    <BottomNavigationAction className={classes.action} label="Prime" />
+                    <BottomNavigationAction className={classes.action} label="1" icon={<ShoppingBasketIcon/>} />  
+                </BottomNavigation>
+            
+            <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+            >
+                <AccountCircle />
+            </IconButton>
+            </div>
+
+            <div className={classes.sectionMobile}>
+                <IconButton
+                    aria-label="show more"
+                    aria-controls={mobileMenuId}
+                    aria-haspopup="true"
+                    onClick={handleMobileMenuOpen}
+                    color="inherit"
+                >
+                    <MoreIcon />
+                </IconButton>
+            </div>
+        </Toolbar>
+    </AppBar>
+    {renderMobileMenu}
+    {renderMenu}
+    </div>
+  );
+}
