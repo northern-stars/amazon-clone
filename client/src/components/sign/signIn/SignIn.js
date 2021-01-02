11d505084a808d0c46bb297cef9235c1b3781cdf
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2021-01-02 19:51:54 +0300

    converted english format

--- client/src/components/sign/signIn/SignIn.js
+++ client/src/components/sign/signIn/SignIn.js
@@ -15,23 +15,23 @@ export default function SignIn() {
                     <Grid item xs={12} sm={5} md={3}>
                         <Paper id="paper"> 
                             <form class="signIn-form" method="POST" action="">
-                                <h1 class="form-header" >Giriş yap</h1>
-                                <label for="signIn-form">E-posta adresi</label>
+                                <h1 class="form-header" >Sign-In</h1>
+                                <label for="signIn-form">Email</label>
                                 <input class="form-input" type="email" name="userEmailSignIn" required/>
-                                <button class="form-button" type="submit">Devam Et</button>  
+                                <button class="form-button" type="submit">Continue</button>  
                             </form>
                             <div class="section-text">
-                                Oturum açarak, Amazon'un <a href="/gp/help/customer/display.htmlref=ap_signin_notification_condition_of_use?ie=UTF8&amp;nodeId=201909000">Kullanım ve SatışKoşulları'nı</a> kabul etmiş olursunuz. Kişisel verilerinizin Amazon tarafından nasıl işlendiğineilişkin detaylı bilgi için <a href="">Gizlilik Bildirimi</a>, <a href="/gp/help/customer/displayhtml/?nodeId=201890250">Çerez Bildirimi</a> ve <a href="/gp/BIT/InternetBasedAds">İlgi AlanınaDayalı Tanıtımları</a> inceleyebilirsiniz. 
+                                By continuing, you agree to Amazon's  <a href="/gp/help/customer/display.htmlref=ap_signin_notification_condition_of_use?ie=UTF8&amp;nodeId=201909000"> Conditions of Use and Privacy Notice.</a>
                             </div>
                             <div class="section-text">
-                                <a href="">Yardıma mı ihtiyacınız var?</a>
+                                <a href="">Need help?</a>
                             </div>
                         </Paper>
                         <div class="divider">
-                                <h5>Amazon hesabınız yok mu?</h5>
+                                <h5>New to Amazon?</h5>
                         </div>
                         <button class="create-button" type="button">
-                            <a href="/signup">Amazon hesabınızı oluşturun</a>
+                            <a href="/signup">Create your Amazon account</a>
                         </button>
                     </Grid>
                 </Grid>
